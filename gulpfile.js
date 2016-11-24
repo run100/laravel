2ae3c681c8e2d9643486acc1763a3ced89d4aa42
run <727271755@qq.com>
2016-11-24 15:33:29 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var elixir = require('laravel-elixir');
+//var elixir = require('laravel-elixir');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +11,71 @@ var elixir = require('laravel-elixir');
  |
  */
 
+
+
+var gulp       = require('gulp'),
+    elixir     = require('laravel-elixir'),
+    ext        = require('gulp-ext-replace'),
+    uglify     = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cssmin     = require('gulp-cssmin');
+
+require('laravel-elixir-remove');
+
+
+var Task = elixir.Task;
+
+elixir.extend('uglify', function(sourceFile, outputFolder)
+{
+    new Task('uglify', function()
+    {
+        return gulp.src(sourceFile)
+            .pipe(sourcemaps.init())
+            .pipe(uglify())
+            .pipe(ext('.min.js'))
+            .pipe(gulp.dest(outputFolder))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest(outputFolder));
+    }).watch([sourceFile]);
+});
+
+elixir.extend('cssmin', function(sourceFile, outputFolder)
+{
+    new Task('cssmin', function()
+    {
+        return gulp.src(sourceFile)
+            .pipe(sourcemaps.init())
+            .pipe(cssmin())
+            .pipe(ext('.min.css'))
+            .pipe(gulp.dest(outputFolder))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest(outputFolder));
+    }).watch([sourceFile]);
+});
+
+
 elixir(function(mix) {
     mix.sass('app.scss');
 });
+
+
+/*
+elixir(function(mix) {
+    mix.copy('resources/assets/js/crm.js', 'public/assets/js/');
+    mix.copy('resources/assets/css/crm.css', 'public/assets/css/');
+    mix.uglify('resources/assets/js/crm.js', 'public/assets/js/');
+
+    mix.uglify('resources/assets/js/laravel.validate.js', 'public/assets/js/');
+
+    mix.uglify('resources/assets/js/call/japp.js', 'public/assets/js/');
+
+    mix.uglify('resources/assets/js/call/jbardisplay.js', 'public/assets/js/');
+
+    mix.uglify('resources/assets/js/call/core.js', 'public/assets/js/');
+
+    mix.cssmin('resources/assets/css/crm.css', 'public/assets/css/');
+
+    elixir(function(mix) {
+        mix.version(['assets/css/crm.css', 'assets/css/crm.min.css', 'assets/js/crm.js', 'assets/js/crm.min.js']);
+    });
+});*/
