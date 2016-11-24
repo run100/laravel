91b44579e6318aa987259375224b54db15494f91
run <727271755@qq.com>
2016-11-24 15:54:00 +0800

    routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/hello', function(){
+    return view('hello');
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
